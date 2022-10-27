c89d853f85cd402ba567157f4cf9dbc5f35f874e
Samuel La <lathanhthai1905@gmail.com>
2022-10-27 18:24:54 +0700

    Initial Unity project

---
+++ Assets/_JustPong/Source/GameEvents/GameEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ultilities
+{
+    [CreateAssetMenu(
+        fileName = "GameEvent", menuName = "Scriptable Objects/Event", order = 1
+    )]
+    public class GameEvent : ScriptableObject
+    {
+        #region Private Fields
+
+        private HashSet<GameEventListener> listeners = new HashSet<GameEventListener>();
+
+        #endregion
+
+
+        #region Public Callbacks
+
+        public virtual void RegisterListener(GameEventListener listener) => listeners.Add(listener);
+
+        public virtual void UnregisterListener(GameEventListener listener) => listeners.Remove(listener);
+
+        public void RaiseEvent()
+        {
+            foreach (var listener in listeners)
+            {
+                listener.Raise();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/_JustPong/Source/GameEvents/GameEventListener.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Ultilities
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        #region Serialized Fields
+
+        [SerializeField]
+        protected GameEvent _event;
+        [SerializeField]
+        protected UnityEvent responses;
+
+        #endregion
+
+
+        #region MonoBehaviour Callbacks
+
+        protected void OnEnable() => _event.RegisterListener(this);
+
+        protected void OnDisable() => _event.UnregisterListener(this);
+
+        #endregion
+
+
+        #region Public Callbacks
+
+        public virtual void Raise() => responses?.Invoke();
+
+        #endregion
+    }
+}
